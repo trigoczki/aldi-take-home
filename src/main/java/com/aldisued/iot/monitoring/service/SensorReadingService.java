c5b505039a3eafe7819dde633fd6ddfc95d8f6a5
trigoczki <rigoczkitamas@gmail.com>
2026-05-18 14:16:35 +0200

    RT: feat: Task3

--- src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
@@ -1,6 +1,7 @@
 package com.aldisued.iot.monitoring.service;
 
 import com.aldisued.iot.monitoring.dto.SensorReadingDto;
+import com.aldisued.iot.monitoring.entity.Sensor;
 import com.aldisued.iot.monitoring.entity.SensorReading;
 import com.aldisued.iot.monitoring.repository.SensorReadingRepository;
 import com.aldisued.iot.monitoring.repository.SensorRepository;
@@ -9,18 +10,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class SensorReadingService {
 
-  private final SensorReadingRepository sensorReadingRepository;
-  private final SensorRepository sensorRepository;
+    private final SensorReadingRepository sensorReadingRepository;
+    private final SensorRepository sensorRepository;
 
-  public SensorReadingService(SensorReadingRepository sensorReadingRepository,
-      SensorRepository sensorRepository) {
-    this.sensorReadingRepository = sensorReadingRepository;
-    this.sensorRepository = sensorRepository;
-  }
+    public SensorReadingService(SensorReadingRepository sensorReadingRepository,
+                                SensorRepository sensorRepository) {
+        this.sensorReadingRepository = sensorReadingRepository;
+        this.sensorRepository = sensorRepository;
+    }
 
-  public SensorReading saveSensorReading(SensorReadingDto sensorReadingDto) {
-    //TODO: Task 3
-    return null;
-  }
+    public SensorReading saveSensorReading(SensorReadingDto sensorReadingDto) {
+        Sensor sensor = sensorRepository.findById(sensorReadingDto.sensorId()).orElseThrow();
+        SensorReading sensorReading = new SensorReading(sensorReadingDto.value(), sensorReadingDto.timestamp(), sensor);
+
+        return sensorReadingRepository.save(sensorReading);
+    }
 
 }
